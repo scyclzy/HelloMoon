d154e0bfe65c1ee3af7a60fd83f65efe7d6e9523
lizhongyuan <261056702@qq.com>
2016-07-24 20:26:15 +0800

    create HelloMoonFragment

---
+++ src/com/example/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,27 @@
+package com.example.hellomoon;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HelloMoonFragment extends Fragment {
+
+	private Button mPlayButton;
+	private Button mStopButton;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		
+		View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
+		mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+		mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+		
+		return v;
+	}
+
+}
